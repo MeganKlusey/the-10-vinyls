42e36275a635a094caa95a6c3ec2434eb4453b68
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-08-03 14:33:25 +0100

    Basic template setup

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
