88d58cf07afb3c7ea8c85b4c15cce2611b92e7c3
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-08-03 15:35:32 +0100

    Import album component into home

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <AlbumComponent />
   </div>
 </template>
 
 <script>
+import AlbumComponent from './Home/AlbumComponent'
+
 export default {
   name: 'HomeComponent',
+  components: {
+    AlbumComponent
+  }
 }
 </script>
 
