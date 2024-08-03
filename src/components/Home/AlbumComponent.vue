e155b843cc11908c4a620a4d9e6c7d426725de2f
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-08-03 15:58:31 +0100

    Album sizing and skeleton

--- src/components/Home/AlbumComponent.vue
+++ src/components/Home/AlbumComponent.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h1>ALBUM</h1>
+  <div class="album">
+    <h2>1</h2>
+    <img id="1" class="album-cover" alt="Album cover" src="../../assets/1.jpg">
+    <h3>The Miseducation of Lauryn Hill</h3>
+    <p>Lauryn Hill</p>
   </div>
 </template>
 
@@ -11,4 +14,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.album {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .album-cover {
+    border-radius: 5px;
+    width: 100%;
+    max-width: 200px;
+    transition-duration: 0.1s;
+    &:hover {
+      transform: skew(-4deg, 4deg);
+    }
+  }
+}
 </style>
