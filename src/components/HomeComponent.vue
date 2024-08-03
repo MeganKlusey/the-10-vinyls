4db23aa5e5cb5ed5ce8efabd4e269123f4b79084
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-08-03 15:01:19 +0100

    Remove default props and text

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeComponent',
-  props: {
-    msg: String
-  }
 }
 </script>
 
