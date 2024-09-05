91ac23dca10de08af253659b804335dd85c96b33
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-09-05 22:21:50 +0100

    Create quantity component

---
+++ src/components/Home/QuantityComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="quantity">
+    <button>-</button>
+    <input />
+    <button>+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuantityComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+.quantity {
+  margin-top: 4px;
+
+  button {
+    background-color: #C48D00;
+  }
+}
+</style>
