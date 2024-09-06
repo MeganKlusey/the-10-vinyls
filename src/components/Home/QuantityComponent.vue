a4c78b409af8c4d3ca93adc03e8c133431d83031
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-09-06 22:01:47 +0100

    Quantity button and input styling

--- src/components/Home/QuantityComponent.vue
+++ src/components/Home/QuantityComponent.vue
@@ -16,8 +16,22 @@ export default {
 .quantity {
   margin-top: 4px;
 
+  input {
+    height: 22px;
+    border: 1px solid #78716C;
+    padding: 0;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
   button {
     background-color: #C48D00;
+    border: none;
+    height: 24px;
+    width: 24px;
+    color: #FFF;
   }
 }
 </style>
