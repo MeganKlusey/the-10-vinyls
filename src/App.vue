612e474074d7ef199fe052c98fef0b212c7c255a
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-09-07 20:33:22 +0100

    Add basket button and position header contents

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <img id="logo" alt="The 10 Vinyls logo" src="./assets/logo.png">
+  <header>
+    <img id="logo" alt="The 10 Vinyls logo" src="./assets/logo.png">
+    <a id="basket" href="/basket">Basket ()</a>
+  </header>
   <HomeComponent />
 </template>
 
@@ -11,10 +14,36 @@ import HomeComponent from './components/HomeComponent.vue'
 body {
   transition-duration: 0.4s;
 
-  #logo {
-    display: block;
-    margin: 0 auto;
-    width: 150px;
+  header {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    #logo {
+      display: block;
+      margin: 0 auto;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 150px;
+    }
+
+    #basket {
+      position: absolute;
+      top: 50%;
+      left: 98%;
+      transform: translate(-98%, -50%);
+      white-space: nowrap;
+      font-family: 'Helvetica', sans-serif;
+      font-weight: 400;
+      margin: 0;
+      text-transform: uppercase;
+      color: #000;
+      letter-spacing: 2px;
+      text-decoration: none;
+    }
   }
 }
 </style>
