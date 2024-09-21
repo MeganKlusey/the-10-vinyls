f50603be21b9fe457f6c1848c6f5452470ee2ed3
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-09-21 17:09:22 +0100

    Empty basket styling

--- src/views/BasketView.vue
+++ src/views/BasketView.vue
@@ -1,11 +1,44 @@
 <template>
-  BASKET!
+    <ul v-if="store.albums.filter(album => album.quantity).length > 0">
+      <li
+        v-bind:key="album.name"
+        v-for="album in store.albums.filter(album => album.quantity > 0)"
+        >
+        <AlbumComponent
+          :name=album.name
+          :artist=album.artist
+          :colour=album.colour
+          :id=album.id
+        />
+      </li>
+    </ul>
+  <div v-if="store.albums.filter(album => album.quantity).length == 0"
+  class="empty-basket-container">
+    <p>Your basket is empty.</p>
+  </div>
 </template>
 
 <script setup>
-
+import { store } from '../store.js'
+import AlbumComponent from './Home/AlbumComponent'
 </script>
 
 <style lang="scss" scoped>
+.empty-basket-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 116px);
 
+  p {
+    font-family: 'Helvetica', sans-serif;
+    font-weight: 400;
+    margin: 0;
+    text-align: center;
+  }
+}
+
+ul li {
+  list-decoration: none;
+}
 </style>
