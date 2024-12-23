08f6cb78baa86669e2ccd1dd3a112b9c12e1b4c4
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-12-23 20:36:34 +0000

    Update public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/mymedium/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/the-10-vinyls/" : "/",
 })
